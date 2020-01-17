b8dd22aca10bfcc17fb0f4fe1bb4c9c24dddc4cc
Francesco Picetti <picettifrancesco@gmail.com>
2020-01-17 17:55:36 +0100

    Added scipy reference

--- GenericSolver/notebooks/unit_tests/Utest_LSQRsolver.py
+++ GenericSolver/notebooks/unit_tests/Utest_LSQRsolver.py
@@ -7,8 +7,15 @@ from pyLinearSolver import LSQRsolver, LCGsolver
 from pyProblem import ProblemL2Linear
 from pyStopper import BasicStopper
 import numpy as np
-
-
+try:
+    from scipy.sparse.linalg import lsqr
+except ImportError:
+    import subprocess
+    import sys
+    subprocess.call([sys.executable, "-m", "pip", "install", "--user", "scipy"])
+    from scipy.sparse.linalg import lsqr
+    
+    
 if __name__ == "__main__":
     # same as scipy (https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.lsqr.html)
     model = pyVector.vectorIC(np.zeros(2, dtype=float))
@@ -20,12 +27,15 @@ if __name__ == "__main__":
     CG = LCGsolver(BasicStopper(niter=1000))
     CG.setDefaults()
     CG.run(L2ProbCG, verbose=False)
-    print(L2ProbCG.model.getNdArray())  # should be near [1, -1]
+    print('CG result: \t\t', L2ProbCG.model.getNdArray())  # should be near [1, -1]
+    
+    # LSQR from scipy
+    print('scipy result: \t',
+          lsqr(np.array([[1., 0.], [1., 1.], [0., 1.]], dtype=float), np.array([1., 0.01, -1.], dtype=float))[0])
     
     # LSQR
     L2Prob = ProblemL2Linear(model, data, A)
     LSQR = LSQRsolver(BasicStopper(niter=1000))
     LSQR.setDefaults()
     LSQR.run(L2Prob, verbose=False)
-    print(L2Prob.model.getNdArray())  # should be near [1, -1]
-
+    print('LSQR result: \t', L2Prob.model.getNdArray())  # should be near [1, -1]
