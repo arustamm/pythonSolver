27c4e04cb44770c486ac2aef69eea933e9dce16a
Ettore Biondi <ettore88@stanford.edu>
2020-03-04 10:47:57 -0800

    Added overriding of save_grad in SplitBregman run method (fixed bug)

--- GenericSolver/python/pySparseSolver.py
+++ GenericSolver/python/pySparseSolver.py
@@ -509,7 +509,7 @@ class SplitBregmanSolver(Solver):
         self.create_msg = verbose or self.logger
 
         # overriding save_grad variable
-        problem.sav_grad = False
+        problem.save_grad = False
         
         # reset stopper before running the inversion
         self.stopper.reset()
