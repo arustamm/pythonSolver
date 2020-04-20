740b93adb2ac026819897c5699bb72d675d7c0ff
Ettore Biondi <ettore88@stanford.edu>
2020-04-19 19:38:54 -0700

    Added LSF Cluster to dask interface

--- GenericSolver/python/dask_util.py
+++ GenericSolver/python/dask_util.py
@@ -9,7 +9,7 @@ import json
 
 DEVNULL = open(os.devnull, 'wb')
 import dask.distributed as daskD
-from dask_jobqueue import PBSCluster
+from dask_jobqueue import PBSCluster, LSFCluster
 
 
 def get_tcp_info(filename):
@@ -24,44 +24,78 @@ def get_tcp_info(filename):
 
 def create_hostnames(machine_names, Nworkers):
     """
-	Function to create hostnames variables (i.e., list of ip addresses)
-	from machine names and number of wokers per machine
-	"""
-    
+    Function to create hostnames variables (i.e., list of ip addresses)
+    from machine names and number of wokers per machine
+    """
+
     ip_adds = []
     for host in machine_names:
         ip_adds.append(socket.gethostbyname(host))
-    
+
     if len(Nworkers) != len(ip_adds):
         raise ValueError("Lenght of number of workers (%s) not consistent with number of machines available (%s)"
                          % (len(Nworkers), len(ip_adds)))
-    
+
     hostnames = []
     for idx, ip in enumerate(ip_adds):
         hostnames += [ip] * Nworkers[idx]
     return hostnames
 
 
+def client_startup(cluster, n_jobs, total_workers):
+    """
+    Function to start a client
+    """
+    if n_jobs <= 0:
+        raise ValueError("n_jobs must equal or greater than 1!")
+    cluster.scale(jobs=n_jobs)
+    # Creating dask Client
+    client = daskD.Client(cluster)
+    workers = 0
+    t0 = time.time()
+    while workers < total_workers:
+        workers = len(client.get_worker_logs().keys())
+        # If the number of workers is not reached in 5 minutes raise exception
+        if time.time() - t0 > 300.0:
+            raise SystemError(
+                "Dask could not start the requested workers within 5 minutes! Try different hostnames.")
+    WorkerIds = list(client.get_worker_logs().keys())
+    return client, WorkerIds
+
+
 class DaskClient:
     """
-	Class useful to construct a Dask Client to be used with Dask vectors and operators
-	"""
-    
+    Class useful to construct a Dask Client to be used with Dask vectors and operators
+    """
+
     def __init__(self, **kwargs):
         """
-		Constructor for obtaining a client to be used when Dask is necesary
-		1) Cluster with shared file system and ssh capability:
-		:param hostnames : - list; list of strings containing the host names or IP addresses of the machines that
-			the user wants to use in their cluster/client (First hostname will be running the scheduler!) [None]
-		:param scheduler_file_prefix : string; prefix to used to create dask scheduler-file.
-			Must be a mounted path on all the machines. Necessary if hostnames are provided [$HOME/scheduler-]
-		2) PBS cluster:
-		:param pbs_params : - dict; dictionary containing PBS Cluster options (see help(PBSCluster) for help) [None]
-		:param n_workers : - int; number of workers to be submitted to the cluster or to be used on each job (if n_jobs is provided)
-		:param n_jobs : - int; number of jobs to be submitted to the cluster; if provided you will have n_workers per job (i.e., n_workers*n_jobs = dask_workers) [None]
-		"""
+    Constructor for obtaining a client to be used when Dask is necesary
+    1) Cluster with shared file system and ssh capability:
+    :param hostnames : - list; list of strings containing the host names or IP addresses of the machines that
+    the user wants to use in their cluster/client (First hostname will be running the scheduler!) [None]
+    :param scheduler_file_prefix : string; prefix to used to create dask scheduler-file.
+    Must be a mounted path on all the machines. Necessary if hostnames are provided [$HOME/scheduler-]
+    2) PBS cluster:
+    :param pbs_params : - dict; dictionary containing PBS Cluster options (see help(PBSCluster) for help) [None]
+    :param n_jobs : - int; number of jobs to be submitted to the cluster
+    :param n_workers: - int; number of workers per job [1]
+    3) LSF cluster:
+    :param lfs_params : - dict; dictionary containing LSF Cluster options (see help(LSFCluster) for help) [None]
+    :param n_workers : - int; number of workers to be submitted to the cluster
+    :param n_workers: - int; number of workers per job [1]
+    """
         hostnames = kwargs.get("hostnames", None)
         pbs_params = kwargs.get("pbs_params", None)
+        lsf_params = kwargs.get("lsf_params", None)
+        ClusterInit = None
+        cluster_params = None
+        if pbs_params:
+            cluster_params = pbs_params
+            ClusterInit = PBSCluster
+        elif lsf_params:
+            cluster_params = lsf_params
+            ClusterInit = LSFCluster
         # Checking interface to be used
         if hostnames:
             if not isinstance(hostnames, list):
@@ -115,54 +149,38 @@ class DaskClient:
                 self.WorkerIds.append(wrkIds[idx])
                 wrkIds.pop(idx)
                 wrk_ips.pop(idx)
-        elif pbs_params:
-            n_workers = kwargs.get("n_workers", 0)
-            n_jobs = kwargs.get("n_jobs", None)
-            if n_workers <= 0:
-                raise ValueError("n_workers must equal or greater than 1!")
-            # if n_jobs is provided then start n_workers on n_jobs
-            if n_jobs:
-                if n_jobs <= 0:
-                    raise ValueError("n_jobs must equal or greater than 1!")
-                pbs_params.update({"processes": n_workers})
-                if n_workers > 1:
-                    # forcing nanny to be true (otherwise, dask-worker command will fail)
-                    pbs_params.update({"nanny": True})
-                n_workers *= n_jobs
-            else:
-                n_jobs = n_workers
-            self.cluster = PBSCluster(**pbs_params)
-            self.cluster.scale(jobs=n_jobs)
-            # Creating dask Client
-            self.client = daskD.Client(self.cluster)
-            workers = 0
-            t0 = time.time()
-            while workers < n_workers:
-                workers = len(self.client.get_worker_logs().keys())
-                # If the number of workers is not reached in 5 minutes raise exception
-                if time.time() - t0 > 300.0:
-                    raise SystemError(
-                        "Dask could not start the requested workers within 5 minutes! Try different hostnames.")
-            self.WorkerIds = list(self.client.get_worker_logs().keys())
+        elif ClusterInit:
+            n_wrks = kwargs.get("n_wrks", 1)
+            n_jobs = kwargs.get("n_jobs")
+            if n_wrks <= 0:
+                raise ValueError("n_wrks must equal or greater than 1!")
+            if n_jobs <= 0:
+                raise ValueError("n_jobs must equal or greater than 1!")
+            cluster_params.update({"processes": n_wrks})
+            if n_wrks > 1:
+                # forcing nanny to be true (otherwise, dask-worker command will fail)
+                cluster_params.update({"nanny": True})
+            self.cluster = ClusterInit(**cluster_params)
+            self.client, self.WorkerIds = client_startup(self.cluster, n_jobs, n_jobs*n_wrks)
         else:
-            raise ValueError("Either hostnames or pbs_params must be provided!")
+            raise ValueError("Either hostnames or pbs_params or lsf_params must be provided!")
         # Closing dask processes
         atexit.register(self.client.shutdown)
-    
+
     def getClient(self):
         """
-		Accessor for obtaining the client object
-		"""
+    Accessor for obtaining the client object
+    """
         return self.client
-    
+
     def getWorkerIds(self):
         """
-		Accessor for obtaining the worker IDs
-		"""
+    Accessor for obtaining the worker IDs
+    """
         return self.WorkerIds
-    
+
     def getNworkers(self):
         """
-		Accessor for obtaining the number of workers
-		"""
+    Accessor for obtaining the number of workers
+    """
         return len(self.getWorkerIds())
