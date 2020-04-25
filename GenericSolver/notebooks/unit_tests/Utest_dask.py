868cd85147d12d40b01e4c1d948043d478a728d4
Ettore Biondi <ettore88@stanford.edu>
2020-04-25 15:40:37 -0700

    Added DaskCollectOp to pyDaskOperator; Minor revision of DaskVector class

--- GenericSolver/notebooks/unit_tests/Utest_dask.py
+++ GenericSolver/notebooks/unit_tests/Utest_dask.py
@@ -5,15 +5,15 @@ import pyOperator as pyOp
 import pyVector as  Vec
 import numpy as np
 
-# hostnames = ["thanos", "thanos", "vision", "vision"]
-# client = DaskClient(hostnames=hostnames)
-params = {"nanny": True, "job_name":"test1", "cores":32, "memory":'1GB', "queue":'xaginolimit', "walltime":'20:00:00', "interface":'ib0', "job_extra": ["-V"]}
+hostnames = ["thanos", "thanos", "friday", "friday"]
+client = DaskClient(hostnames=hostnames)
+# params = {"nanny": True, "job_name":"test1", "cores":32, "memory":'1GB', "queue":'xaginolimit', "walltime":'20:00:00', "interface":'ib0', "job_extra": ["-V"]}
 # params = {"nanny":"0", "job_name":"test1","cores":32, "memory":'120GB', "queue":'xagigpu', "walltime":'9999:00:00', "interface":'ib0', "resource_spec":"select=1:ncpus=32:mem=120gb:ngpus=2"}
 # client = DaskClient(pbs_params=params,n_workers=2,n_jobs=2)
-client = DaskClient(pbs_params=params,n_workers=2)
-vec = Vec.vectorIC((100,10))
+# client = DaskClient(pbs_params=params,n_workers=2)
+vec = Vec.vectorIC((10,1))
 chunks = (1,1,1,1)
-vecD = pyDaskVector.DaskVector(client,vector_template=vec,chunks=chunks)
+vecD = pyDaskVector.DaskVector(client, vector_template=vec, chunks=chunks)
 
 # Checking which worker has a given chunk
 client.getClient().who_has(vecD.vecDask[0])
@@ -21,9 +21,20 @@ client.getClient().who_has(vecD.vecDask[1])
 client.getClient().who_has(vecD.vecDask[2])
 client.getClient().who_has(vecD.vecDask[3])
 
+# Testing DaskCollectOp
+vecLocal = Vec.vectorIC(np.block(vecD.getNdArray()).shape)
+
+CollectOp = DaskOp.DaskCollectOp(vecD,vecLocal)
+CollectOp.dotTest(True)
+vecD.rand()
+CollectOp.forward(False, vecD, vecLocal)
+print(vecLocal.getNdArray()[-1][-1])
+print(vecD.getNdArray()[-1][-1][-1])
+
+
 vec1 = Vec.vectorIC((200,1))
 vectors = [vec1.clone(),vec1.clone()]
-vec1D = pyDaskVector.DaskVector(client,vectors=vectors)
+vec1D = pyDaskVector.DaskVector(client, vectors=vectors)
 
 vec1D.rand()
 vec1D.norm()
