ca33ec2393fc78a12d76fcc7e0648eb23f0c4cce
Francesco Picetti <francesco.picetti@polimi.it>
2020-07-05 13:15:40 +0200

    Added Gradient operator; added stencil type in FirstDerivative
    removed TotalVariation and Fourier

--- GenericSolver/python/pyNpOperator.py
+++ GenericSolver/python/pyNpOperator.py
@@ -4,12 +4,9 @@ from __future__ import division, print_function, absolute_import
 import numpy as np
 import pyVector as pyVec
 import pyOperator as pyOp
-
 import sep_util
 from scipy.signal import convolve, correlate
 from scipy.ndimage import gaussian_filter
-from scipy.sparse.linalg import LinearOperator
-from itertools import product
 
 
 class MatrixOp(pyOp.Operator):
@@ -71,6 +68,168 @@ class MatrixOp(pyOp.Operator):
 
 
 class FirstDerivative(pyOp.Operator):
+    def __init__(self, model, sampling=1., axis=0, kind='centered'):
+        r"""
+        Compute 2nd order centered first derivative
+
+        .. math::
+            y[i] = 0.5 (x[i+1] - x[i-1]) / dx
+
+        :param model    : vector class; domain vector
+        :param sampling : scalar; sampling step [1.]
+        :param axis     : int; axis along which to compute the derivative [0]
+        :param kind     : str; derivative kind (centered, forward, backward)
+        """
+        self.sampling = sampling
+        self.dims = model.getNdArray().shape
+        self.axis = axis if axis >= 0 else len(self.dims) + axis
+        self.kind = kind
+        
+        if self.kind == 'centered':
+            self.forward = self._forwardC
+            self.adjoint = self._adjointC
+        elif self.kind == 'backward':
+            self.forward = self._forwardB
+            self.adjoint = self._adjointB
+        elif self.kind == 'forward':
+            self.forward = self._forwardF
+            self.adjoint = self._adjointF
+        else:
+            raise ValueError("Derivative kind must be centered, forward or backward")
+        
+        super(FirstDerivative, self).__init__(model, model)
+    
+    def __str__(self):
+        return "1stDer_%d" % self.axis
+    
+    def _forwardF(self, add, model, data):
+        """Forward operator for the 1st order forward stencil"""
+        self.checkDomainRange(model, data)
+        if add:
+            data_tmp = data.clone()
+        data.zero()
+        # Getting Ndarrays
+        x = model.clone().getNdArray()
+        if self.axis > 0:  # need to bring the dim. to derive to first dim
+            x = np.swapaxes(x, self.axis, 0)
+        y = np.zeros(x.shape)
+    
+        y[:-1] = (x[1:] - x[:-1]) / self.sampling
+        if self.axis > 0:  # reset axis order
+            y = np.swapaxes(y, 0, self.axis)
+        data.getNdArray()[:] = y
+        if add:
+            data.scaleAdd(data_tmp)
+        return
+    
+    def _adjointF(self, add, model, data):
+        """Adjoint operator for the 1st order forward stencil"""
+        self.checkDomainRange(model, data)
+        if add:
+            model_temp = model.clone()
+        model.zero()
+        # Getting Ndarrays
+        y = data.clone().getNdArray()
+        if self.axis > 0:  # need to bring the dim. to derive to first dim
+            y = np.swapaxes(y, self.axis, 0)
+        x = np.zeros(y.shape)
+        
+        x[:-1] -= y[:-1] / self.sampling
+        x[1:] += y[:-1] / self.sampling
+        
+        if self.axis > 0:
+            x = np.swapaxes(x, 0, self.axis)
+        model.getNdArray()[:] = x
+        if add:
+            model.scaleAdd(model_temp)
+        return
+
+    def _forwardC(self, add, model, data):
+        """Forward operator for the 2nd order centered stencil"""
+        self.checkDomainRange(model, data)
+        if add:
+            data_tmp = data.clone()
+        data.zero()
+        # Getting Ndarrays
+        x = model.clone().getNdArray()
+        if self.axis > 0:  # need to bring the dim. to derive to first dim
+            x = np.swapaxes(x, self.axis, 0)
+        y = np.zeros(x.shape)
+    
+        y[1:-1] = (.5 * x[2:] - 0.5 * x[:-2]) / self.sampling
+        if self.axis > 0:  # reset axis order
+            y = np.swapaxes(y, 0, self.axis)
+        data.getNdArray()[:] = y
+        if add:
+            data.scaleAdd(data_tmp)
+        return
+
+    def _adjointC(self, add, model, data):
+        """Adjoint operator for the 2nd order centered stencil"""
+        self.checkDomainRange(model, data)
+        if add:
+            model_temp = model.clone()
+        model.zero()
+        # Getting Ndarrays
+        y = data.clone().getNdArray()
+        if self.axis > 0:  # need to bring the dim. to derive to first dim
+            y = np.swapaxes(y, self.axis, 0)
+        x = np.zeros(y.shape)
+    
+        x[:-2] -= 0.5 * y[1:-1] / self.sampling
+        x[2:] += 0.5 * y[1:-1] / self.sampling
+    
+        if self.axis > 0:
+            x = np.swapaxes(x, 0, self.axis)
+        model.getNdArray()[:] = x
+        if add:
+            model.scaleAdd(model_temp)
+        return
+
+    def _forwardB(self, add, model, data):
+        """Forward operator for the 1st order backward stencil"""
+        self.checkDomainRange(model, data)
+        if add:
+            data_tmp = data.clone()
+        data.zero()
+        # Getting Ndarrays
+        x = model.clone().getNdArray()
+        if self.axis > 0:  # need to bring the dim. to derive to first dim
+            x = np.swapaxes(x, self.axis, 0)
+        y = np.zeros(x.shape)
+    
+        y[1:] = (x[1:] - x[:-1]) / self.sampling
+        if self.axis > 0:  # reset axis order
+            y = np.swapaxes(y, 0, self.axis)
+        data.getNdArray()[:] = y
+        if add:
+            data.scaleAdd(data_tmp)
+        return
+
+    def _adjointB(self, add, model, data):
+        """Adjoint operator for the 1st order backward stencil"""
+        self.checkDomainRange(model, data)
+        if add:
+            model_temp = model.clone()
+        model.zero()
+        # Getting Ndarrays
+        y = data.clone().getNdArray()
+        if self.axis > 0:  # need to bring the dim. to derive to first dim
+            y = np.swapaxes(y, self.axis, 0)
+        x = np.zeros(y.shape)
+    
+        x[:-1] -= y[1:] / self.sampling
+        x[1:] += y[1:] / self.sampling
+    
+        if self.axis > 0:
+            x = np.swapaxes(x, 0, self.axis)
+        model.getNdArray()[:] = x
+        if add:
+            model.scaleAdd(model_temp)
+        return
+    
+
+class FirstDerivativeOld(pyOp.Operator):
     def __init__(self, model, sampling=1., axis=0):
         r"""
         Compute 2nd order centered first derivative
@@ -85,7 +244,7 @@ class FirstDerivative(pyOp.Operator):
         self.sampling = sampling
         self.dims = model.getNdArray().shape
         self.axis = axis if axis >= 0 else len(self.dims) + axis
-        super(FirstDerivative, self).__init__(model, model)
+        super(FirstDerivativeOld, self).__init__(model, model)
     
     def __str__(self):
         return "1stDer_%d" % self.axis
@@ -103,7 +262,6 @@ class FirstDerivative(pyOp.Operator):
         y = np.zeros(x.shape)
         
         y[:-1] = (x[1:] - x[:-1]) / self.sampling
-        y[-1] = 0
         if self.axis > 0:  # reset axis order
             y = np.swapaxes(y, 0, self.axis)
         data.getNdArray()[:] = y
@@ -203,63 +361,47 @@ class SecondDerivative(pyOp.Operator):
         return
 
 
-class TotalVariation(pyOp.Operator):
-    def __init__(self, model, axis=None, weights=None, sampling=None, iso=False):
+class Gradient(pyOp.Operator):
+    def __init__(self, model, sampling=None):
         r"""
-        (An)isotropic Total Variation operator.
-        The input parameters are tailored for >2D, but it works also for 1D.
+        N-Dimensional Gradient operator.
 
         :param model    : vector class; domain vector
-        :param axis     : tuple; axis along which to compute the derivative [0, 1]
-        :param weights  : tuple; scalar weight for the axis [1, 1]
-        :param sampling : tuple; sampling step [1, 1]
-        :param iso      : bool; compute isotropic operator [False]
+        :param sampling : tuple; sampling step [1]
         """
         self.dims = model.getNdArray().shape
-        self.axis = axis if axis is not None else tuple(range(len(self.dims)))
         self.sampling = sampling if sampling is not None else tuple([1] * len(self.dims))
-        self.weights = weights if weights is not None else tuple([1] * len(self.dims))
         
-        assert len(self.axis) == len(self.weights) == len(self.sampling) != 0, \
-            "There is something wrong with the dimensions"
+        assert len(self.sampling) != 0, "There is something wrong with the dimensions"
         
-        self.isotropic = iso
-        
-        if self.isotropic:  # self.op is a list of operators
-            self.op = [self.weights[d] * FirstDerivative(model, sampling=self.sampling[d], axis=self.axis[d]) for d in
-                       range(len(self.axis))]
-        else:  # self.op is itself the final operator
-            self.op = self.weights[0] * FirstDerivative(model, sampling=self.sampling[0], axis=self.axis[0])
-            for d in range(1, len(self.axis)):
-                self.op += self.weights[d] * FirstDerivative(model, sampling=self.sampling[d], axis=self.axis[d])
-        
-        super(TotalVariation, self).__init__(model, model)
+        self.op = pyOp.Vstack([FirstDerivative(model, sampling=self.sampling[d], axis=d)
+                               for d in range(len(self.dims))])
+        super(Gradient, self).__init__(domain=self.op.domain, range=self.op.range)
     
     def __str__(self):
-        return "TotalVar"
+        return "Gradient"
     
     def forward(self, add, model, data):
-        if self.isotropic:
-            self.checkDomainRange(model, data)
-            if add:
-                data_tmp = data.clone()
-            data.zero()
-            for op in self.op:
-                temp = data.clone().zero()
-                op.forward(False, model, temp)
-                data.scaleAdd(temp.pow(2))
-            data.pow(.5)
-            if add:
-                data.scaleAdd(data_tmp)
-            return
-        else:
-            return self.op.forward(add, model, data)
+        return self.op.forward(add, model, data)
     
     def adjoint(self, add, model, data):
-        if self.isotropic:
-            raise ValueError("ERROR! The Isotropic Total Variation is nonlinear.")
+        return self.op.adjoint(add, model, data)
+    
+    def merge_directions(self, add, model, data, iso=True):
+        """
+        Merge the different directional contributes, using the L2 norm (iso=True) or the simple sum (iso=False)
+        """
+        self.range.checkSame(model)
+        if not add:
+            data.zero()
+        
+        if iso:
+            for v in model.vecs:
+                data.scaleAdd(v.clone().pow(2), 1., 1.)
+                data.pow(.5)
         else:
-            return self.op.adjoint(add, model, data)
+            for v in model.vecs:
+                data.scaleAdd(v, 1., 1.)
 
 
 class Laplacian(pyOp.Operator):
@@ -296,8 +438,8 @@ class Laplacian(pyOp.Operator):
     
     def adjoint(self, add, model, data):
         return self.op.adjoint(add, model, data)
-
-
+        
+        
 class GaussianFilter(pyOp.Operator):
     def __init__(self, model, sigma):
         """
@@ -388,86 +530,6 @@ class ConvNDscipy(pyOp.Operator):
         modelNd = model.getNdArray()
         dataNd = data.getNdArray()[:]
         modelNd += correlate(dataNd, self.kernel, mode='same', method=self.method)
-        return
-
-
-def FourierTransform(domain, dirs=None, nfft=None, sampling=1., dtype=np.complex128):
-    if isinstance(domain, pyVec.vectorIC):
-        return _FFT_IC(domain, dirs, nfft, sampling, dtype)
-    elif isinstance(domain, pyVec.superVector):
-        # TODO add the possibility to have different settings for each sub-vector
-        return pyOp.Dstack([_FFT_IC(v, dirs, nfft, sampling, dtype) for v in domain.vecs])
-    else:
-        raise ValueError("ERROR! Provided domain has to be either vector or superVector")
-
-
-class _FFT_IC(pyOp.Operator):
-    
-    def __init__(self, domain, dirs=None, nfft=None, sampling=1., dtype=np.complex128):
-        """
-        :param domain   : vector class; domain vector
-        :param dirs     : scalar of sequence of scalars
-                            directions along which to compute the FFT
-        :param nfft    : scalar or sequence of scalars
-                            number of FFT points
-        :param sampling : scalar or sequence of scalars
-                            sampling step for FFT computation
-        :return         : FFT Operator
-        """
-        assert isinstance(domain, pyVec.vectorIC), "Domain has to be a vectorIC object (for now)"
-        self.dims = domain.shape
-        if dirs is None:
-            self.dirs = list(range(len(self.dims)))
-        else:
-            self.dirs = [int(dirs[_]) for _ in range(len(dirs))]
-            assert len(self.dirs) <= len(
-                self.dims), "The number of FFT directions is greater than the domain dimensionality"
-        
-        if nfft is None:
-            self.nffts = self.dims
-        else:
-            self.nffts = [int(nfft[_]) for _ in range(len(nfft))]
-            assert len(self.nffts) <= len(
-                self.dims), "The number of FFT points is greater than the domain dimensionality"
-        
-        if np.isscalar(sampling):
-            self.sampling = [sampling] * len(self.nffts)
-        else:
-            self.sampling = list(sampling)
-            assert len(self.sampling) <= len(
-                self.nffts), "The number of FFT samplings is greater than the domain dimensionality"
-        
-        assert len(self.nffts) == len(self.sampling) == len(self.dirs), "Provided parameters dimensions mismatch"
-        
-        super(_FFT_IC, self).__init__(domain, pyVec.vectorIC(np.zeros(self.nffts, dtype=dtype)))
-        
-        # store vectors of frequency bins
-        self.weight = np.sqrt(np.prod(self.nffts))
-        self.sampling_frequencies = [np.fft.fftfreq(nfft, d=samp) for nfft, samp in zip(self.nffts, self.sampling)]
-    
-    def __str__(self):
-        return "Fourier "
-    
-    def forward(self, add, model, data):
-        """Compute FFT on model and save to data"""
-        self.checkDomainRange(model, data)
-        if not add:
-            data.zero()
-        data.arr += 1. / self.weight * np.fft.fftn(model.getNdArray(), s=self.nffts, axes=self.dirs)
-        return
-    
-    def adjoint(self, add, model, data):
-        """Compute IFFT on data and save to model"""
-        self.checkDomainRange(model, data)
-        if add:
-            temp = model.clone()
-        model_arr = self.weight * np.fft.ifftn(data.getNdArray(), s=self.nffts, axes=self.dirs)
-        for direction in self.dirs:
-            model_arr = np.take(model_arr, range(self.dims[direction]), axis=direction)
-        
-        model.arr = model_arr
-        if add:
-            model.scaleAdd(temp, 1., 1.)
         return
 
 
@@ -563,15 +625,5 @@ if __name__ == '__main__':
     # xx = pyVec.superVector(x, x)
     # PP = ZeroPad(xx, pad)
     # PP.dotTest()
-    
-    # Test FFT
-    F = FourierTransform(x, nfft=[512, 1024])
-    X = F * x
-    F.dotTest(True)
 
-    xx = pyVec.superVector(x, x)
-    FF = FourierTransform(xx, nfft=[512, 1024])
-    yyfft = FF * yy
-    FF.dotTest(True)
-    #
     # print(0)
