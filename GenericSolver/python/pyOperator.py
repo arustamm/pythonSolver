fc9be2d8f54906ec016ff027bc2912e30119b215
Ettore Biondi <ettore88@stanford.edu>
2020-08-10 18:22:09 -0700

    Added sum of non-linear operators

--- GenericSolver/python/pyOperator.py
+++ GenericSolver/python/pyOperator.py
@@ -367,7 +367,7 @@ class _sumOperator(Operator):
         self.args = (A, B)
 
     def __str__(self):
-        return self.args[0].__str__()[:3] + "+" + self.args[0].__str__()[:4]
+        return self.args[0].__str__()[:3] + "+" + self.args[1].__str__()[:4]
 
     def forward(self, add, model, data):
         self.checkDomainRange(model, data)
@@ -397,7 +397,7 @@ class _prodOperator(Operator):
         self.temp = B.getRange().clone()
 
     def __str__(self):
-        return self.args[0].__str__()[:3] + "*" + self.args[0].__str__()[:4]
+        return self.args[0].__str__()[:3] + "*" + self.args[1].__str__()[:4]
 
     def forward(self, add, model, data):
         self.checkDomainRange(model, data)
@@ -802,6 +802,13 @@ class NonLinearOperator(Operator):
             plt.show()
         return alpha, lin_err
 
+    # unary operators
+    def __add__(self, other):  # self + other
+        if isinstance(other, NonLinearOperator):
+            return _sumNlOperator(self, other)
+        else:
+            raise TypeError('Argument must be an Operator')
+
 
 class _combNonLinearOperator(NonLinearOperator):
     """
@@ -842,6 +849,46 @@ class _combNonLinearOperator(NonLinearOperator):
 def CombNonlinearOp(g, f):
     """Combination of non-linear opeartors: f(g(m))"""
     return _combNonLinearOperator(f, g)
+
+
+class _sumNlOperator(NonLinearOperator):
+    """
+    Sum of two non-linear operators
+        h = g + f
+    """
+
+    def __init__(self, g, f):
+        """Sum operator constructor"""
+        if not isinstance(g, NonLinearOperator) or not isinstance(f, NonLinearOperator):
+            raise TypeError('Both operands have to be a NonLinearOperator')
+        if not f.range.checkSame(g.range) or not f.domain.checkSame(g.domain):
+            raise ValueError('Cannot add operators: shape mismatch')
+
+        self.args = (g, f)
+        # Defining f(g(m))
+        self.nl_op = _sumOperator(f.nl_op, g.nl_op)
+        # Defining F(g(m0))G(m0)
+        self.lin_op = _sumOperator(f.lin_op, g.lin_op)
+        # Defining internal set_background functions
+        self.set_background_f = f.set_background
+        self.set_background_g = g.set_background
+        # Defining non_linear operator g(m) for Jacobian definition
+        self.g_nl_op = g.nl_op
+        self.g_range_tmp = g.nl_op.range.clone()
+        super(_sumNlOperator, self).__init__(self.nl_op, self.lin_op, self.set_background)
+
+    def __str__(self):
+        return self.args[0].__str__()[:3] + "+" + self.args[1].__str__()[:4]
+
+    def set_background(self, model):
+        """
+        Set background function for the sum of Jacobian matrices
+        """
+        # Setting G(m0)
+        self.set_background_g(model)
+        # Setting F(m0)
+        self.set_background_f(model)
+
 
 
 class VstackNonLinearOperator(NonLinearOperator):
@@ -1062,7 +1109,6 @@ def main():
     cosJac = cosJacobian(x)
     cosNl = NonLinearOperator(cosOp, cosJac, cosJac.set_background)
     cosNl.linTest(x.rand(), plot=True)
-
 
 if __name__ == '__main__':
     main()
