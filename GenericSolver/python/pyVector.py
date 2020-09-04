fe58764975b5ab4019e01014927421cc2912cace
Ettore Biondi <ettore88@stanford.edu>
2020-09-04 10:50:37 -0700

    Fixed bug in writeVec and pySolver

--- GenericSolver/python/pyVector.py
+++ GenericSolver/python/pyVector.py
@@ -346,8 +346,12 @@ class vectorSet:
         """Method to write to SEPlib file (by default it appends vectors to file)"""
         if mode not in "aw":
             raise ValueError("ERROR! mode must be either a (append) or w (write)")
-        for vec_i in self.vecSet:
-            vec_i.writeVec(filename, mode)
+        for iVec, vec_i in enumerate(self.vecSet):
+            if mode == "w" and iVec == 0:
+                wr_mode = "w"
+            else:
+                wr_mode = "a"
+            vec_i.writeVec(filename, wr_mode)
         self.vecSet = []  # List of vectors of the set
 
 
