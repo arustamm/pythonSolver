0f1f5bf97e3af708e8bc33dc831a043e27d946cf
Rustam Akhmadiev <akhmarust@gmail.com>
2023-04-28 13:48:01 -0700

    test fwd

--- GenericSolver/python/__pyDaskOperator.py
+++ GenericSolver/python/__pyDaskOperator.py
@@ -74,15 +74,16 @@ class DaskOperator(DaskObject, Operator.Operator):
         waitable = [f for sublist in res for f in sublist]
         wait(waitable)
         # accumulate 
-        # fin = []
-        # fin.append(dat)
+        # TODO look here this causes memory blowup
+        fin = []
+        fin.append(dat)
         # for i, d in enumerate(res):
         #     dd = self.client.map(data.cls.__add__, fin[i], d, pure=False)
         #     fin.append(dd)
-        # # copy the futures
-        # dd = self.client.map(data.cls.scaleAdd, dat, pure=False)
-        # data.set_futures(fin[-1])
-        # del res, fin
+        # copy the futures
+        # dd = self.client.map(data.cls.clone, dat, pure=False)
+        data.set_futures(fin[-1])
+        del res, fin
 
     def adjoint(self, add, model, data):
 
