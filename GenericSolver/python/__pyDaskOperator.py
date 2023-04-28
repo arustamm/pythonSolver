3413f04f0d1eb62e359e0da0070df06f227cb204
Rustam Akhmadiev <akhmarust@gmail.com>
2023-04-28 16:38:17 -0700

    test fwd

--- GenericSolver/python/__pyDaskOperator.py
+++ GenericSolver/python/__pyDaskOperator.py
@@ -77,11 +77,11 @@ class DaskOperator(DaskObject, Operator.Operator):
         fin = []
         fin.append(dat)
         for i, d in enumerate(res):
-            dd = self.client.map(data.cls.__add__, fin[i], d, pure=False)
+            dd = self.client.map(data.cls.__add__, fin[i], d, pure=True)
             fin.append(dd)
         # copy the futures
-        dd = self.client.map(data.cls.clone, fin[-1], pure=False)
-        data.set_futures(dd)
+        # dd = self.client.map(data.cls.clone, dat, pure=False)
+        data.set_futures(fin[-1])
         del res, fin
 
     def adjoint(self, add, model, data):
